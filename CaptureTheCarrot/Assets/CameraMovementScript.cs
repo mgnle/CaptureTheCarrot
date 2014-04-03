daa816732880a9a67a897ddc705ecb87c012495b
mgnle <matt.giannelli@gmail.com>
2014-04-02 21:17:21 -0400

    Added Unity files with basic RTS camera

---
+++ CaptureTheCarrot/Assets/CameraMovementScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovementScript : MonoBehaviour {
+
+	public int scrollDistance = 5; 
+	public float scrollSpeed = 70;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Move Camera Script
+	float horizontalSpeed = 40f;
+	float verticalSpeed = 40f;
+
+	// Update is called once per frame
+	void Update () {
+		
+		float mousePosX = Input.mousePosition.x; 
+		float mousePosY = Input.mousePosition.y; 
+
+		
+		if (mousePosX < scrollDistance) 
+		{ 
+			transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime); 
+		} 
+		
+		if (mousePosX >= Screen.width - scrollDistance) 
+		{ 
+			transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime); 
+		}
+		
+		if (mousePosY < scrollDistance) 
+		{ 
+			transform.Translate(transform.forward * -scrollSpeed * Time.deltaTime); 
+		} 
+		
+		if (mousePosY >= Screen.height - scrollDistance) 
+		{ 
+			transform.Translate(transform.forward * scrollSpeed * Time.deltaTime); 
+		}
+	}
+}
