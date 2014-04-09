b21b046dff7e192e84740099351f0dd3ce6da4db
mgnle <matt.giannelli@gmail.com>
2014-04-09 12:47:45 -0400

    Added basic bunny controller
    If you right click, the bunnies should move forward

---
+++ CaptureTheCarrot/Assets/Scripts/BunnyControl.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController))]
+
+public class BunnyControl : MonoBehaviour {
+
+	// Movement properties
+	public float moveSpeed;
+
+	// Debug variables
+	public bool isMoving;
+
+	// Movement vector
+	public Vector3 moveVector = Vector3.zero;
+
+	CharacterController controller;
+	CollisionFlags collisionFlags;
+	bool isGrounded;
+
+	// initial position
+	Vector3 initialPosition;
+	Quaternion initialRotation;
+	Vector3 horizontalVelocity;
+	
+	// Use this for initialization
+	void Start () {
+		controller = gameObject.GetComponent<CharacterController>();
+
+		initialPosition = transform.position;
+		initialRotation = transform.rotation;
+
+		isMoving = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(1)) {
+			moveVector = new Vector3(1f, -0.001f, 0f);
+			moveVector = transform.TransformDirection(moveVector);
+			moveVector *= moveSpeed;
+			isMoving = true;
+		}
+		else {
+			moveVector = new Vector3(0f, -0.001f, 0f);
+			isMoving = false;
+		}
+
+		collisionFlags = controller.Move(moveVector * Time.deltaTime);	
+	}
+}
