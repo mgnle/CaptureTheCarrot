a74d352076ee0c42902748f188301bbc649b113b
mgnle <matt.giannelli@gmail.com>
2014-04-09 18:18:30 -0400

    Added bunny movement functions
    added functions MoveLeft(), MoveRight(), MoveStraight()

--- CaptureTheCarrot/Assets/Scripts/BunnyControl.cs
+++ CaptureTheCarrot/Assets/Scripts/BunnyControl.cs
@@ -6,10 +6,11 @@ using System.Collections;
 public class BunnyControl : MonoBehaviour {
 
 	// Movement properties
-	public float moveSpeed;
+	public float moveDistance;
+	public float rotationAngle;
 
-	// Debug variables
-	public bool isMoving;
+	// Access variables
+	public Vector3 bunnyPos;
 
 	// Movement vector
 	public Vector3 moveVector = Vector3.zero;
@@ -21,7 +22,6 @@ public class BunnyControl : MonoBehaviour {
 	// initial position
 	Vector3 initialPosition;
 	Quaternion initialRotation;
-	Vector3 horizontalVelocity;
 	
 	// Use this for initialization
 	void Start () {
@@ -29,23 +29,37 @@ public class BunnyControl : MonoBehaviour {
 
 		initialPosition = transform.position;
 		initialRotation = transform.rotation;
-
-		isMoving = false;
+		bunnyPos = initialPosition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButton(1)) {
-			moveVector = new Vector3(1f, -0.001f, 0f);
-			moveVector = transform.TransformDirection(moveVector);
-			moveVector *= moveSpeed;
-			isMoving = true;
+		if (Input.GetMouseButtonDown(1)) {
+			MoveStraight();
 		}
-		else {
-			moveVector = new Vector3(0f, -0.001f, 0f);
-			isMoving = false;
-		}
+		bunnyPos = transform.position;
+	}
 
-		collisionFlags = controller.Move(moveVector * Time.deltaTime);	
+	/* Rotates bunny to the right and moves forward */
+	void MoveRight() {
+		transform.Rotate(0f, rotationAngle, 0f);
+		Vector3 oldPos = transform.position;
+		moveVector = transform.forward * moveDistance;
+		transform.position = oldPos + moveVector;
+	}
+
+	/* Rotates bunny to the left and moves forward */
+	void MoveLeft() {
+		transform.Rotate(0f, -rotationAngle, 0f);
+		Vector3 oldPos = transform.position;
+		moveVector = transform.forward * moveDistance;
+		transform.position = oldPos + moveVector;
+	}
+
+	/* Moves bunny forward */
+	void MoveStraight() {
+		Vector3 oldPos = transform.position;
+		moveVector = transform.forward * moveDistance;
+		transform.position = oldPos + moveVector;
 	}
 }
