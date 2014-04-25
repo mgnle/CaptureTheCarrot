30959e3b34e8ef5bb036c021fbf5a67687272912
mgnle <matt.giannelli@gmail.com>
2014-04-25 06:12:44 -0400

    Added training reward sliders to GUI
    Each represent floats, and can by accessed in TrainingScript by:
    gui.carrotProximityReward
    gui.enemyProximityReward
    gui.enemyAttackReward
    gui.mudProximityReward

--- CaptureTheCarrot/Assets/Scripts/TrainingGUIScript.cs
+++ CaptureTheCarrot/Assets/Scripts/TrainingGUIScript.cs
@@ -11,8 +11,18 @@ public class TrainingGUIScript : MonoBehaviour {
 		Mud
 	}
 
+	// Item to be placed
 	public Item selectedItem;
 	int numItemTypes = 4;
+	
+	// Current values of sliders
+	public float carrotProximityReward;
+	public float carrotProximityDistance;
+	public float enemyProximityReward;
+	public float enemyProximityDistance;
+	public float mudProximityReward;
+	public float mudProximityDistance;
+	public float enemyAttackReward;
 
 	public Texture carrotIcon;
 	public Texture bunnyIcon;
@@ -21,7 +31,6 @@ public class TrainingGUIScript : MonoBehaviour {
 
 	public int boxDim = 80;
 
-	GUIText selectionLabel;
 	string selectionText = "";
 
 	Texture[] icons;
@@ -30,7 +39,13 @@ public class TrainingGUIScript : MonoBehaviour {
 	void Start () {
 		selectedItem = (Item)0;
 		icons = new Texture[] {carrotIcon, bunnyIcon, enemyBunnyIcon, mudIcon};
-		selectionLabel = GameObject.Find ("Selection Label").guiText;
+		carrotProximityReward = 0.0f;
+		carrotProximityDistance = 0.0f;
+		enemyProximityReward = 0.0f;
+		enemyProximityDistance = 0.0f;
+		mudProximityReward = 0.0f;
+		mudProximityDistance = 0.0f;
+		enemyAttackReward = 0.0f;
 	}
 
 	void OnGUI () {
@@ -38,6 +53,7 @@ public class TrainingGUIScript : MonoBehaviour {
 		GUI.backgroundColor = Color.red;
 		selectedItem = (Item) (GUI.SelectionGrid(new Rect(Screen.width/2 - 60*(numItemTypes/2), Screen.height - 80, boxDim*numItemTypes, boxDim), (int)selectedItem, icons, numItemTypes, "button"));
 
+		// Item selector
 		switch(selectedItem)
 		{
 		case Item.Bunny:
@@ -54,7 +70,22 @@ public class TrainingGUIScript : MonoBehaviour {
 			break;
 		}
 
-		selectionLabel.text = selectionText;
+		GUI.Box (new Rect(Screen.width/2 - 20, Screen.height - 110, 120, 25), selectionText);
+		
+		// Sliders
+		enemyAttackReward = GUI.VerticalSlider(new Rect(60, Screen.height - 80, 20, 70), enemyAttackReward, 100f, -100f);
+		GUI.Box (new Rect(10, Screen.height - 110, 120, 25), "Attack Enemy");
+		GUI.Box (new Rect(25, Screen.height - 55, 30, 25), Mathf.Floor(enemyAttackReward).ToString());
+		carrotProximityReward = GUI.VerticalSlider(new Rect(190, Screen.height - 80, 20, 70), carrotProximityReward, 100f, -100f);
+		GUI.Box (new Rect(140, Screen.height - 110, 120, 25), "Approach Carrot");
+		GUI.Box (new Rect(155, Screen.height - 55, 30, 25), Mathf.Floor(carrotProximityReward).ToString());
+		mudProximityReward = GUI.VerticalSlider(new Rect(320, Screen.height - 80, 20, 70), mudProximityReward, 100f, -100f);
+		GUI.Box (new Rect(270, Screen.height - 110, 120, 25), "Approach Mud");
+		GUI.Box (new Rect(285, Screen.height - 55, 30, 25), Mathf.Floor(mudProximityReward).ToString());
+		enemyProximityReward = GUI.VerticalSlider(new Rect(450, Screen.height - 80, 20, 70), enemyProximityReward, 100f, -100f);
+		GUI.Box (new Rect(400, Screen.height - 110, 120, 25), "Approach Enemy");
+		GUI.Box (new Rect(415, Screen.height - 55, 30, 25), Mathf.Floor(enemyProximityReward).ToString());
+		
 	}
 
 	public void IncrementSelection() {
@@ -68,7 +99,7 @@ public class TrainingGUIScript : MonoBehaviour {
 		if ((int)selectedItem > 0)
 			selectedItem--;
 		else
-			selectedItem = (Item)numItemTypes;
+			selectedItem = (Item)numItemTypes - 1;
 	}
 
 	// Update is called once per frame
