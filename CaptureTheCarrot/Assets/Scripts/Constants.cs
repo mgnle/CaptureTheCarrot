5d57fe6d7759ab17505b9880409fef245cc03d8e
Naomi Robert <naomi.robert@gatech.edu>
2014-04-29 18:35:44 -0400

    Changed probabilities in Constants

--- CaptureTheCarrot/Assets/Scripts/Constants.cs
+++ CaptureTheCarrot/Assets/Scripts/Constants.cs
@@ -3,12 +3,12 @@ namespace AssemblyCSharp
 {
 		public class Constants
 		{
-				public const int TIME_ALIVE_THRESHOLD = 30;
+				public const int TIME_ALIVE_THRESHOLD = 25;
 		
-				public const double PROBABILITY_MUTATE_WEIGHT = 1;
+				public const double PROBABILITY_MUTATE_WEIGHT = 0.8;
 				public const int AMOUNT_MUTATE_WEIGHT = 5;	// In hundredths
-				public const double PROBABILITY_ADD_NODE = 1;
-				public const double PROBABILITY_ADD_CONNECTION = 1;
+				public const double PROBABILITY_ADD_NODE = 0.7;
+				public const double PROBABILITY_ADD_CONNECTION = 0.8;
 		}
 }
 
